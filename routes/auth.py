a5dda3cf398242e9f4ad746785b521a39a3d498d
Jacky Cheng <chengjc@umich.edu>
2025-11-24 22:29:58 -0500

    Fix missing get_db import

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from models.user import User
+from models.db import get_db
+
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
